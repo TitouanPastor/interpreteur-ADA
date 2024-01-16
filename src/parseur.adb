c044ddaf22f128eb0af8bbd9f678c21cb034e05d
togram13 <margot.bonafos@gmail.com>
2024-01-16 17:54:04 +0100

    Begin of parseur adb and ads

--- src/parseur.adb
+++ src/parseur.adb
@@ -11,12 +11,14 @@ package body parseur is
       Ligne   : Unbounded_String;
 
    begin
-
+      
+      -- Ouverture du fichier
       Open (Fichier, In_File, chemin);
 
       -- Lire et afficher chaque ligne du fichier
       Put_Line(" ");
       Put_Line("Fichier à parser : ");
+      
       while not End_Of_File (Fichier) loop
          Get_Line (Fichier, Ligne);
          
@@ -25,11 +27,110 @@ package body parseur is
 
       Put_Line("####################################");
       Put_Line(" ");
+      
+      while not End_Of_File (Fichier) loop
+         
+         -- A1 : Récupérer la ligne sous forme d'un tableau de mots
+         
+         Get_Line (Fichier, ligne);
+         
+         LigneToTabMot(ligne, tabMots);
+         
+         --  A2 : On traite la ligne courante
+         
+         If tabMots(0) = "Programme" then
+            
+            y := 0;
+            
+            while tabMots(0) /= "Début" loop
+               
+               -- On récupère la ligne suivante
+               
+               
+               Put_Line (Ligne);
 
-      Close (File => Fichier);
-
-
+               -- passe la ligne sous forme d'un tableau de mots 
+               
+               LigneToTabMot(ligne, tabMots);
+               
+               -- B1 : On ajoute les variables au tas
+               
+               i:= 0;
+               
+               -- Boucle qui parcoure tout le tableau de mots
+               while i /= tabMot.nbElements loop
+                  
+                  -- On vérifie si on ets arrivé au bout des variables
+                  
+                  while tabMot.tabMotLigne[i] /= ':' loop
+                     
+                     AjouterVariable (tas, tabMot.tabMotLigne[i]);
+                     i := i+1;
+                  end loop;
+                  
+               end loop;
+               
+      end loop;
+      
+      
+      -- Fermeture du fichier
+      Close (File => Fichier); 
 
    end FichierToMemoire;
+   
+     --------------------
+   -- InitialiserTabMot --
+     --------------------
+
+   procedure InitialiserTabMot (TabMot : out T_Tab_Mot) is
+   begin
+      TabMot.nbElements := 0;
+   end InitialiserTabMot;
+   
+   
+     --------------------
+   -- InitialisercntVar --
+     --------------------
+
+   procedure InitialisercntVar (compteur : out Integer) is
+   begin
+      compteur := 0;
+   end InitialisercntVar;
+   
+     --------------------
+   -- InitialiserDico --
+     --------------------
+
+   procedure InitialiserDico (Map : out String_Hashed_Maps.Map) is
+   begin
+      String_Hashed_Maps.Clear(Map);
+   end InitialiserDico;
+   
+   
+   
+    --------------------
+   -- LigneToTabMot --
+     --------------------
+
+   procedure LigneToTabMot (ligneCourante : in String; tabMot : out T_Mot) is
+      
+   begin
+      
+      -- Parcourir la chaîne jusqu'à la fin (caractère nul)
+      
+      i := 0;
+      
+      while ligneCourante(Index) /= Character'Val(0) loop
+         
+         tabMot.tabMotLigne[i] := ligneCourante(i);
+         Ada.Text_IO.Put(ligneCourante(i));
+         i := i + 1;
+         
+      end loop;
+            
+      tabMot.nbElements := i;
+      
+   end LigneToTabMot;
+
 
 end parseur;
--- src/parseur.ads
+++ src/parseur.ads
@@ -8,19 +8,76 @@ with Dico_entiers; use Dico_entiers;
 
 package parseur is
 
+
+   CAPACITE : Integer := 1000;  -- Capacité maximale du Tableau de mots
+
+   type T_Tab_Mot is private;  -- Type privé pour le tableau de mots
+
+   type T_Mot is record -- Type enregistrement pour le tableau de mots
+      tabMotLigne    : T_Tab_Mot;  -- Tableau de mots correspondant à la ligne
+      nbElements : Integer;  -- Nombre d'éléments actuellement dans le tableau
+   end record;
+
+
    -- Nom fonction : FichierToMemoire
 
    -- Sémantique : Lit un fichier texte contenant le code intermediaire et le traduit en mémoire (T_Tas et T_Memoire_Code)
 
    -- Paramètres :
-   -- chemin : String : chemin du fichier texte
-   -- memVar : out T_Tas : tas contenant les variables
-   -- memCode : out T_Memoire_Code : mémoire contenant le code intermediaire
-   -- dico_entiers : out Integer_Hashed_Maps.Map : table de correspondance entre les variables et leurs valeurs
+   --   - chemin : String : chemin du fichier texte
+   --   - memVar : out T_Tas : tas contenant les variables
+   --   - memCode : out T_Memoire_Code : mémoire contenant le code intermediaire
+   --   - dico_entiers : out Integer_Hashed_Maps.Map : table de correspondance entre les variables et leurs valeurs
 
    -- Pre  => chemin /= null,
    -- Post => memVar /= null, memCode /= null;
    procedure FichierToMemoire
      (chemin : in String; memVar : out T_Tas; memCode : out T_Memoire_Code; dico_entiers : out Integer_Hashed_Maps.Map);
+
+
+   -- Nom fonction : InitialiserTabMot
+
+   -- Sémantique : Initialise le tableau qui permet de stocker chaque mot d'une ligne
+
+   -- Paramètres :
+   --    - TabMot : out T_Tab_Mot --> le tableau de mot correspondant à la ligne courante
+
+   -- Pre  => rien,
+   -- Post => tabMotLigne.nbElements = 0;
+   procedure InitialiserTabMot (tabMot : out T_Tab_Mot);
+
+
+   -- Nom fonction : LigneToTabMot
+
+   -- Sémantique : Prend une ligne en entrée et retourne le tableau TabMot dans lequel il y a un mot de la ligne par case
+
+   -- Paramètres :
+   --    - ligneCourante : in String --> la ligne du code intermédiaire qu'il faut mettre sous forme de mots
+   --    - tabMot : le tableau de mot correspondant à la ligne
+
+   -- Pre  => rien,
+   -- Post => ligneCourante non vide
+   procedure LigneToTabMot (ligneCourante : in String; tabMot : out T_Tab_Mot );
+
+
+     -- Nom fonction : InitialisercntVar
+
+   -- Sémantique : Initialise le compteur de variables
+
+   -- Paramètres :
+   --    - compteur : out T_Dico--> le compteur de variables
+
+   -- Pre  => rien,
+   -- Post => ?
+   procedure InitialisercntVar (compteur : out Integer);
+
+
+
+   private
+
+   type T_Tab_Mot is
+     array (1 .. CAPACITE) of String;  -- Définition du type tableau de mots
+
+
 
 end parseur;
