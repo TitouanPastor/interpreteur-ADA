9433f9b0ba2b7e00c0e3be3efba4cfb5c3d2cc60
TitouanPastor <titouan.pastor@gmail.com>
2024-01-18 11:42:05 +0100

    CLeared parseur.adb

--- src/parseur.adb
+++ src/parseur.adb
@@ -31,13 +31,15 @@ package body parseur is
    -- LigneToTabMot --
    --------------------
 
-   procedure LigneToTabMot
-     (ligneCourante : in Unbounded_String; tabMot : out T_Mot)
-   is
-      Mot : Unbounded_String;
+   procedure LigneToTabMots (Fichier : in out File_Type; tabMot : out T_Mot) is
+      Mot           : Unbounded_String;
+      ligneCourante : Unbounded_String;
    begin
       -- On reinitialise le tableau de mots
       InitialiserTabMot (tabMot);
+      -- On récupere la ligne courante
+      Get_Line (Fichier, ligneCourante);
+      Put_Line (ligneCourante);
       -- Parcourir la ligne et extraire les mots
       for Index in 1 .. Length (ligneCourante) loop
          -- Si le caractère n'est pas un espace, ajouter au mot temporaire
@@ -68,7 +70,7 @@ package body parseur is
          Put_Line (tabMot.tabMotLigne (Index));
       end loop;
 
-   end LigneToTabMot;
+   end LigneToTabMots;
 
    procedure InitMapOperations (map : out Integer_Hashed_Maps.Map) is
    begin
@@ -89,6 +91,116 @@ package body parseur is
       map.Include ("AND", 15);
    end InitMapOperations;
 
+   procedure ParserOperation
+     (mapVariable   : in     Integer_Hashed_Maps.Map;
+      mapOperations : in     Integer_Hashed_Maps.Map; tabMots : in T_Mot;
+      memCode       :    out T_Memoire_Code)
+   is
+      x, y : Integer; -- valeur dans les opérations x <- y op z avec y et z pouvant etre des variables ou des constantes
+      xIsVariable, yIsVariable : Integer;
+   begin
+      -- On regarde si la première opérande est une constante ou une variable
+      if mapVariable.Contains (To_String (tabMots.tabMotLigne (4))) then
+         x           := mapVariable (To_String (tabMots.tabMotLigne (4)));
+         xIsVariable := 1;
+      else
+         x           := Integer'Value (To_String (tabMots.tabMotLigne (4)));
+         xIsVariable := 0;
+      end if;
+
+      --Si l'instruction est une simple assignation ( x <- 4)
+      if tabMots.nbElements = 4 then
+         InsererInstruction
+           (memCode, mapVariable (To_String (tabMots.tabMotLigne (2))),
+            xIsVariable, x, mapOperations ("->"), 0, 0);
+      else -- Sinon on regarde la seconde opérande
+
+         if mapVariable.Contains (To_String (tabMots.tabMotLigne (6))) then
+            y           := mapVariable (To_String (tabMots.tabMotLigne (6)));
+            yIsVariable := 1;
+         else
+            y           := Integer'Value (To_String (tabMots.tabMotLigne (6)));
+            yIsVariable := 0;
+         end if;
+         InsererInstruction
+           (memCode, mapVariable (To_String (tabMots.tabMotLigne (2))),
+            xIsVariable, x,
+            mapOperations (To_String (tabMots.tabMotLigne (5))), yIsVariable,
+            y);
+      end if;
+   end ParserOperation;
+
+   procedure ParserGoto (memCode : out T_Memoire_Code; tabMots : in T_Mot) is
+   begin
+      InsererInstruction
+        (memCode, -1, 0, Integer'Value (To_String (tabMots.tabMotLigne (3))),
+         0, 0, 0);
+   end ParserGoto;
+
+   procedure ParserIf
+     (mapVariable : in Integer_Hashed_Maps.Map; memCode : out T_Memoire_Code;
+      tabMots     : in T_Mot)
+   is
+   begin
+      if mapVariable.Contains (To_String (tabMots.tabMotLigne (3))) then
+         InsererInstruction
+           (memCode, -2, 1, mapVariable (To_String (tabMots.tabMotLigne (3))),
+            0, 0, Integer'Value (To_String (tabMots.tabMotLigne (5))));
+      else
+         InsererInstruction
+           (memCode, -2, 0,
+            Integer'Value (To_String (tabMots.tabMotLigne (3))), 0, 0,
+            Integer'Value (To_String (tabMots.tabMotLigne (5))));
+      end if;
+   end ParserIf;
+
+   procedure ParserNull (memCode : out T_Memoire_Code) is
+   begin
+      InsererInstruction (memCode, -3, 0, 0, 0, 0, 0);
+   end ParserNull;
+
+   procedure TraiterDeclarationsVariables
+     (Fichier     : in out File_Type; tabMots : in out T_Mot; tas : out T_Tas;
+      mapVariable : in out Integer_Hashed_Maps.Map)
+   is
+      i          : Integer;
+      cntVar     : Integer;
+      tempNomVar : Unbounded_String;
+   begin
+      Put_Line ("######### Dans la déclaration des variables");
+      InitialisercntVar (cntVar);
+      while tabMots.tabMotLigne (1) /= "Début" loop
+         i := 1;
+         -- Boucle qui parcoure tout le tableau de mots
+         -- On vérifie si on ets arrivé au bout des variables
+         while tabMots.tabMotLigne (i) /= ":" loop
+            AjouterVariable (tas, 0);
+            tempNomVar := tabMots.tabMotLigne (i);
+
+            -- Vérifier si le mot suivant est ":"
+            if i < tabMots.nbElements
+              and then tabMots.tabMotLigne (i + 1) = ":"
+            then
+               -- La variable n'est pas suivie par une virgule
+               mapVariable.Include (To_String (tempNomVar), cntVar);
+            else
+               -- La variable est suivie par une virgule
+               mapVariable.Include
+                 (To_String (tempNomVar) (1 .. Length (tempNomVar) - 1),
+                  cntVar);
+            end if;
+            Put ("Ajout de " & tempNomVar);
+            Put (" à l'indice " & cntVar'Image);
+            Put_Line (" ");
+            cntVar := cntVar + 1;
+            i      := i + 1;
+         end loop;
+         LigneToTabMots (Fichier, tabMots);
+      end loop;
+
+      Put_Line ("######### Sortie déclaration variables");
+   end TraiterDeclarationsVariables;
+
    ----------------------
    -- FichierToMemoire --
    ----------------------
@@ -97,15 +209,10 @@ package body parseur is
      (chemin      : in String; tas : out T_Tas; memCode : out T_Memoire_Code;
       mapVariable :    out Integer_Hashed_Maps.Map)
    is
-      Fichier                  : File_Type;
-      Ligne                    : Unbounded_String;
-      tabMots                  : T_Mot;
-      i                        : Integer;
-      x, y : Integer; -- valeur dans les opérations x <- y op z avec y et z pouvant etre des variables ou des constantes
-      xIsVariable, yIsVariable : Integer;
-      cntVar                   : Integer;
-      tempNomVar               : Unbounded_String;
-      mapOperations            : Integer_Hashed_Maps.Map;
+      Fichier       : File_Type;
+      Ligne         : Unbounded_String;
+      tabMots       : T_Mot;
+      mapOperations : Integer_Hashed_Maps.Map;
 
    begin
 
@@ -114,68 +221,20 @@ package body parseur is
 
       --  Initialisation
       InitialiserTabMot (tabMots);
-      InitialisercntVar (cntVar);
       InitMapOperations (mapOperations);
 
-      --MAIN
+      --MAIN loop
       while not End_Of_File (Fichier) and tabMots.tabMotLigne (1) /= "Fin" loop
 
-         -- A1 : Récupérer la ligne sous forme d'un tableau de mots
+         LigneToTabMots (Fichier, tabMots);
 
-         Get_Line (Fichier, Ligne);
-         Put_Line (Ligne);
-
-         LigneToTabMot (Ligne, tabMots);
-
-         --  A2 : On traite la ligne courante
-
+      -- On traite la ligne courante
+      -- Cas du programme avec la déclaration des variable à la ligne suivante
          if tabMots.tabMotLigne (1) = "Programme" then
 
-            Put_Line ("######### Dans la déclaration des variables");
-            -- On récupère la ligne suivante
-            Get_Line (Fichier, Ligne);
-            Put_Line (Ligne);
-            -- passe la ligne sous forme d'un tableau de mots
-            LigneToTabMot (Ligne, tabMots);
+            LigneToTabMots (Fichier, tabMots);
 
-            while tabMots.tabMotLigne (1) /= "Début" loop
-
-               -- B1 : On ajoute les variables au tas
-
-               i := 1;
-
-               -- Boucle qui parcoure tout le tableau de mots
-               -- On vérifie si on ets arrivé au bout des variables
-               while tabMots.tabMotLigne (i) /= ":" loop
-                  AjouterVariable (tas, 0);
-                  tempNomVar := tabMots.tabMotLigne (i);
-
-                  -- Vérifier si le mot suivant est ":"
-                  if i < tabMots.nbElements
-                    and then tabMots.tabMotLigne (i + 1) = ":"
-                  then
-      -- La variable n'est pas suivie par une virgule, ajustez en conséquence
-                     mapVariable.Include (To_String (tempNomVar), cntVar);
-                  else
-                     -- La variable est suivie par une virgule
-                     mapVariable.Include
-                       (To_String (tempNomVar) (1 .. Length (tempNomVar) - 1),
-                        cntVar);
-                  end if;
-                  Put ("Ajout de " & tempNomVar);
-                  Put (" à l'indice " & cntVar'Image);
-                  Put_Line (" ");
-                  cntVar := cntVar + 1;
-                  i      := i + 1;
-               end loop;
-               -- On récupère la ligne suivante
-               Get_Line (Fichier, Ligne);
-               Put_Line (Ligne);
-               -- passe la ligne sous forme d'un tableau de mots
-               LigneToTabMot (Ligne, tabMots);
-            end loop;
-
-            Put_Line ("######### Sortie déclaration variables");
+            TraiterDeclarationsVariables (Fichier, tabMots, tas, mapVariable);
 
          end if;
 
@@ -183,11 +242,7 @@ package body parseur is
          if tabMots.tabMotLigne (1) = "Début" then
 
             Put_Line ("######### Dans les instructions");
-            -- On récupère la ligne suivante
-            Get_Line (Fichier, Ligne);
-            Put_Line (Ligne);
-            -- passe la ligne sous forme d'un tableau de mots
-            LigneToTabMot (Ligne, tabMots);
+            LigneToTabMots (Fichier, tabMots);
 
             while tabMots.tabMotLigne (1) /= "Fin" loop
 
@@ -197,87 +252,37 @@ package body parseur is
                if mapVariable.Contains (To_String (tabMots.tabMotLigne (2)))
                then
                   Put_Line ("-> OPE");
-      -- On regarde si la première opérande est une constante ou une variable
-                  if mapVariable.Contains (To_String (tabMots.tabMotLigne (4)))
-                  then
-                     x := mapVariable (To_String (tabMots.tabMotLigne (4)));
-                     xIsVariable := 1;
-                  else
-                     x := Integer'Value (To_String (tabMots.tabMotLigne (4)));
-                     xIsVariable := 0;
-                  end if;
+                  ParserOperation
+                    (mapVariable, mapOperations, tabMots, memCode);
 
-                  --Si l'instruction est une simple assignation ( x <- 4)
-                  if tabMots.nbElements = 4 then
-                     InsererInstruction
-                       (memCode,
-                        mapVariable (To_String (tabMots.tabMotLigne (2))),
-                        xIsVariable, x, mapOperations ("->"), 0, 0);
-                  else -- Sinon on regarde la seconde opérande
-
-                     if mapVariable.Contains
-                         (To_String (tabMots.tabMotLigne (6)))
-                     then
-                        y := mapVariable (To_String (tabMots.tabMotLigne (6)));
-                        yIsVariable := 1;
-                     else
-                        y :=
-                          Integer'Value (To_String (tabMots.tabMotLigne (6)));
-                        yIsVariable := 0;
-                     end if;
-                     InsererInstruction
-                       (memCode,
-                        mapVariable (To_String (tabMots.tabMotLigne (2))),
-                        xIsVariable, x,
-                        mapOperations (To_String (tabMots.tabMotLigne (5))),
-                        yIsVariable, y);
-                  end if;
                   -- GOTO
                elsif tabMots.tabMotLigne (2) = To_Unbounded_String ("GOTO")
                then
                   Put_Line ("-> GOTO");
-                  InsererInstruction
-                    (memCode, -1, 0,
-                     Integer'Value (To_String (tabMots.tabMotLigne (3))), 0, 0,
-                     0);
+                  ParserGoto (memCode, tabMots);
 
                   -- IF
                elsif tabMots.tabMotLigne (2) = To_Unbounded_String ("IF") then
                   Put_Line ("-> IF");
-                  if mapVariable.Contains (To_String (tabMots.tabMotLigne (3)))
-                  then
-                     InsererInstruction
-                       (memCode, -2, 1,
-                        mapVariable (To_String (tabMots.tabMotLigne (3))), 0,
-                        0,
-                        Integer'Value (To_String (tabMots.tabMotLigne (5))));
-                  else
-                     InsererInstruction
-                       (memCode, -2, 0,
-                        Integer'Value (To_String (tabMots.tabMotLigne (3))), 0,
-                        0, Integer'Value (To_String (tabMots.tabMotLigne (5))));
-                  end if;
+                  ParserIf (mapVariable, memCode, tabMots);
 
                   -- NULL
                elsif tabMots.tabMotLigne (2) = To_Unbounded_String ("NULL")
                then
                   Put_Line ("-> NULL");
-                  InsererInstruction (memCode, -3, 0, 0, 0, 0, 0);
+                  ParserNull (memCode);
 
                else
                   Put_Line ("-> Non reconnu");
                end if;
 
-               -- On récupère la ligne suivante
-               Get_Line (Fichier, Ligne);
-               Put_Line (Ligne);
-               -- passe la ligne sous forme d'un tableau de mots
-               LigneToTabMot (Ligne, tabMots);
+               LigneToTabMots (Fichier, tabMots);
             end loop;
             Put_Line ("######### Sortie instructions");
          end if;
-
       end loop;
+
+
 
       -- Affichage de la map dico_entiers
       Put_Line ("####################################");
@@ -300,18 +305,11 @@ package body parseur is
       Put_Line ("####################################");
       Put_Line (" ");
       Put_Line ("Affichage de la mémoire de code : ");
-      Put_Line (" ");
 
       -- Affichage de la mémoire de code
       for i in 1 .. getNbInstructions (memCode) loop
          Put (i'Image & " : ");
-         Put (GetCaseInstruction (GetInstruction (memCode, i), 1)'Image);
-         Put (GetCaseInstruction (GetInstruction (memCode, i), 2)'Image);
-         Put (GetCaseInstruction (GetInstruction (memCode, i), 3)'Image);
-         Put (GetCaseInstruction (GetInstruction (memCode, i), 4)'Image);
-         Put (GetCaseInstruction (GetInstruction (memCode, i), 5)'Image);
-         Put (GetCaseInstruction (GetInstruction (memCode, i), 6)'Image);
-         Put_Line (" ");
+         AfficherInstruction(GetInstruction (memCode, i));
       end loop;
 
       Put_Line ("####################################");
