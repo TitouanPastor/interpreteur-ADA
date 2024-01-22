95911ad6faff7d1189a2acaf8d581e4ac466df91
Titouan Pastor <titouan.pastor@gmail.com>
2024-01-22 13:08:44 +0100

    (Margot) : Added menu package

---
+++ src/menu.adb
@@ -0,0 +1,48 @@
+package body Menu is
+
+    ---------------------
+   -- ChoixFichier --
+   ---------------------
+
+   procedure ChoixFichier (chemin : out string) is
+
+   begin
+
+      Put_Line("Merci d'entrer le chemin du fichier sans guillemets");
+      Get(chemin);
+
+   end ChoixFichier;
+
+   ---------------------
+   -- ChoixMode --
+   ---------------------
+
+   function ChoixMode return Integer is
+
+      choix : Integer;
+
+   begin
+
+      Put_Line("Saisir 1 pour le mode normal ou 2 pour le mode debeugueur");
+      Get(choix);
+      return choix;
+
+   end ChoixMode;
+
+   ---------------------
+   -- ChoixSortir --
+   ---------------------
+
+   function ChoixSortir return Integer is
+
+      choix : integer;
+
+   begin
+
+      Put_Line("Saisir 1 pour sortir ou 2 pour executer un nouveau programme");
+      Get(choix);
+      return choix;
+
+   end ChoixSortir;
+
+end Menu;
---
+++ src/menu.ads
@@ -0,0 +1,47 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+  
+package Menu is
+    
+   -- Nom fonction : ChoixMode
+
+   -- Sémantique : Permet à l'utilisateur de saisir le chemin du fichier 
+
+   -- Paramètres :
+   --   chemin    : out string    --> chemin vers le fichier de code intermedaire à executer
+   
+   -- Pre : /
+   -- Post : /
+   procedure ChoixFichier (chemin : out string);
+
+   -- Nom fonction : ChoixMode
+
+   -- Sémantique : Permet à l'utilisateur de choisir le mode d'execution
+
+   -- Paramètres :
+   --   /
+
+   -- retour :
+   --  Integer --> 1 correspondant au mode 1 (mode normal) ou 2 correspondant au mode debuggueur
+
+   -- Pre : /
+   -- Post : /
+   
+   function ChoixMode return Integer;
+    
+   
+   -- Nom fonction : ChoixSortir 
+
+   -- Sémantique : Permet à l'utilisateur de saisir recommencer l'execution d'un nouveau programme ou de sortir
+
+   -- Paramètres :
+   --   /
+   
+   -- retour :
+   --  Integer --> 1 correspondant à sortir du programme ou 2 correspondant à recommencer le programme
+
+   -- Pre : /
+   -- Post : /
+   function ChoixSortir return Integer;
+
+end Menu;
